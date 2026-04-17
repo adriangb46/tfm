025c1339ead1c901f3c5f9a66f06c91053761327
Adrián González Blanco <z3u2thnder@proton.me>
2026-04-17 17:43:04 +0200

    feat: add environment preparation script to clone and install project dependencies

---
+++ prepareEnviroment.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/agb4455/ProyectoIntermodularDamBackend.git db_back
+git clone https://github.com/agb4455/ProyectoIntermodularDamFrontend.git front
+git clone https://github.com/adriangb46/ProyectoIntermodularDamServidorIntermedio.git middle_server
+
+cd front
+npm install
+cd ..
+
+cd middle_server
+npm install
+cd ..
+
